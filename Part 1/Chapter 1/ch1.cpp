9f85da951bdf02d2efd473e793d676a27129e93a
Dennis von Bargen <dennis@vonbargen.se>
2022-11-14 20:38:45 +0100

    Initial commit

---
+++ Part 1/Chapter 1/ch1.cpp
@@ -0,0 +1,18 @@
+//
+// Created by dennis on 2022-11-14.
+//
+
+#include <cstdio>
+#include "ch1.h"
+
+int ch1::absolute_value(int x) {
+    if (x >= 0)
+        return x;
+    return x * -1;
+}
+
+int ch1::main() {
+    int my_num = -10;
+    printf("The absolute value of %d is %d.\n", my_num, absolute_value(my_num));
+    return 0;
+}
---
+++ Part 1/Chapter 1/ch1.h
@@ -0,0 +1,16 @@
+//
+// Created by dennis on 2022-11-14.
+//
+
+#ifndef PART_1_CH1_H
+#define PART_1_CH1_H
+
+
+class ch1 {
+public:
+    static int absolute_value(int x);
+    static int main();
+};
+
+
+#endif //PART_1_CH1_H
